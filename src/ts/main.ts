bad080a22620d83093037354dfda6b61e3f73267
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2021-08-11 10:51:35 +0200

    1.0 : Ajout du darkmode
    Possibilité de choisir entre auto, activé, désactivé

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,9 +1,20 @@
+enum darkmode {
+    auto = 0,
+    enabled = 1,
+    disabled = -1
+}
+
 let start = performance.now();
-window.onload = () =>{
+window.onload = () => {
     let end = performance.now();
+    let colorThemeSwitcher: any = document.getElementById("colorThemeSwitcher");
+    changeTheme(colorThemeSwitcher);
+    window.addEventListener("scroll", updateNav);
+    colorThemeSwitcher.addEventListener("click", () => {changeTheme(colorThemeSwitcher) });
     console.log("Page chargée en " + Math.round(end-start)/1000 + " secondes");
 }
-window.addEventListener("scroll", updateNav);
+
+let DEBUG:boolean = true;
 fetch("assets/html/modals.html")
     .then(response => {
         return response.text()
@@ -12,7 +23,117 @@ fetch("assets/html/modals.html")
         document.body.innerHTML += data;
     });
 
+let darkPref: boolean = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+function getDarkmode(value:String): darkmode{
+    console.log("value : " + value);
+    switch(Number(value)){
+        case darkmode.enabled:
+            if(DEBUG) console.log("get darkmode : " + darkmode.enabled);
+            return darkmode.enabled;
+        case darkmode.disabled:
+            if(DEBUG) console.log("get darkmode : " + darkmode.disabled);
+            return darkmode.disabled;
+        default :
+            if(DEBUG) console.log("get darkmode : " + darkmode.auto);
+            return darkmode.auto;
+    }
+}
+
+function setDarkmode(e:any): void {
+    currentTheme = e;
+    localStorage.removeItem("theme");
+    localStorage.setItem("theme", String(previousDarkmode(e)));
+    if(DEBUG)console.log("set local storage and theme : " + e);
+}
+
+function previousDarkmode(d: darkmode): darkmode{
+    switch(d){
+        case darkmode.auto: return darkmode.disabled;
+        case darkmode.enabled: return darkmode.auto;
+        case darkmode.disabled: return darkmode.enabled;
+        default: return null;
+    }
+}
+
+function nextDarkmode(d: darkmode): darkmode{
+    switch(d){
+        case darkmode.auto: return darkmode.enabled;
+        case darkmode.enabled: return darkmode.disabled;
+        case darkmode.disabled: return darkmode.auto;
+        default: return null;
+    }
+}
+
+let currentTheme: darkmode;
+let localTheme:boolean = false;
+if(localStorage.getItem("theme")) {
+    if(DEBUG)console.log("local storage not null ("+ localStorage.getItem("theme")+")");
+    localTheme = true;
+    currentTheme = getDarkmode(localStorage.getItem("theme"));
+}
+else {
+    if(DEBUG)console.log("local storage null ("+ localStorage.getItem("theme")+")");
+    currentTheme = darkmode.auto;
+}
+function updateDarkmodeButton(e:any): void {
+    switch (currentTheme) {
+        case darkmode.auto:
+            e.innerHTML = '🌗';
+            if(DEBUG)console.log("darkmode : Auto")
+            break;
+        case darkmode.enabled:
+            e.innerHTML = '🌙';
+            if(DEBUG)console.log("darkmode : Dark")
+            break;
+        default:
+            e.innerHTML = '☀️';
+            if(DEBUG)console.log("darkmode : Light")
+            break;
+    }
+}
+
+function changeTheme(e: any): void {
+    let classes = document.querySelectorAll(".darkmode-auto, .darkmode-enabled, .darkmode-disabled");
+    removeDarkmodeClass(classes);
+    addDarkmodeClass(classes);
+    updateNav();
+    updateDarkmodeButton(e);
+}
+
+function removeDarkmodeClass(a: any): void {
+    [].forEach.call(a, function (e: Element) {
+        e.classList.remove("darkmode-auto");
+        e.classList.remove("darkmode-enabled");
+        e.classList.remove("darkmode-disabled");
+    });
+}
+
+function addDarkmodeClass(a: any): void {
+    switch (currentTheme) {
+        case darkmode.auto:
+            [].forEach.call(a, function (e: Element) {
+                e.classList.add("darkmode-enabled");
+            });
+            setDarkmode(darkmode.enabled);
+            break;
+        case darkmode.enabled:
+            [].forEach.call(a, function (e: Element) {
+                e.classList.add("darkmode-disabled");
+            });
+            setDarkmode(darkmode.disabled);
+            break;
+        case darkmode.disabled:
+            [].forEach.call(a, function (e: Element) {
+                e.classList.add("darkmode-auto");
+            });
+            setDarkmode(darkmode.auto);
+            break;
+    }
+}
+
 function updateNav(): void {
+    darkPref = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     const presentation = document.getElementById('presentation');
     const skills = document.getElementById('skills');
     const career = document.getElementById('career');
@@ -20,7 +141,7 @@ function updateNav(): void {
     const contact = document.getElementById('contact');
     let nav = document.getElementById('navigation');
 
-    if (presentation.getBoundingClientRect().bottom > (window.innerWidth/7) * -1) {
+    if (presentation.getBoundingClientRect().bottom > (window.innerWidth / 7) * -1) {
         removeColors(nav);
         nav.classList.add("bg-blue");
         nav.classList.add("navbar-dark");
@@ -28,8 +149,10 @@ function updateNav(): void {
         document.getElementById('presentation-link').classList.add("active");
     } else if (skills.getBoundingClientRect().bottom > (window.innerWidth / 7) * -1) {
         removeColors(nav);
-        nav.classList.add("bg-body");
-        nav.classList.add("navbar-light");
+        if ((currentTheme === darkmode.auto && darkPref) || currentTheme === darkmode.enabled) nav.classList.add("bg-dark");
+        else nav.classList.add("bg-light");
+        if ((currentTheme === darkmode.auto && darkPref) || currentTheme === darkmode.enabled) nav.classList.add("navbar-dark");
+        else nav.classList.add("navbar-light");
         removeActiveLink();
         document.getElementById('skills-link').classList.add("active");
     } else if (career.getBoundingClientRect().bottom > (window.innerWidth / 7) * -1) {
@@ -40,8 +163,10 @@ function updateNav(): void {
         document.getElementById('career-link').classList.add("active");
     } else if (portfolio.getBoundingClientRect().bottom > (window.innerWidth / 7) * -1) {
         removeColors(nav);
-        nav.classList.add("bg-body");
-        nav.classList.add("navbar-light");
+        if ((currentTheme === darkmode.auto && darkPref) || currentTheme === darkmode.enabled) nav.classList.add("bg-dark");
+        else nav.classList.add("bg-light");
+        if ((currentTheme === darkmode.auto && darkPref) || currentTheme === darkmode.enabled) nav.classList.add("navbar-dark");
+        else nav.classList.add("navbar-light");
         removeActiveLink();
         document.getElementById('portfolio-link').classList.add("active");
     } else {
@@ -53,7 +178,7 @@ function updateNav(): void {
     }
 }
 
-function removeColors(e: Element):void {
+function removeColors(e: Element): void {
     e.classList.remove("bg-blue");
     e.classList.remove("bg-red");
     e.classList.remove("bg-gray-900");
@@ -68,14 +193,13 @@ function removeColors(e: Element):void {
     e.classList.remove("text-secondary");
     e.classList.remove("text-dark");
     e.classList.remove("text-light");
-    e.classList.remove("text-body");
     e.classList.remove("text-white");
     e.classList.remove("text-muted");
     e.classList.remove("navbar-light");
     e.classList.remove("navbar-dark");
 }
 
-function removeActiveLink():void {
+function removeActiveLink(): void {
     let actives = document.querySelectorAll('.active');
     [].forEach.call(actives, function (e: Element) {
         e.classList.remove("active");
